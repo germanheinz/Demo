31684a47ec20bfe1a78bf3f359981fa75e0478e2
extern-german-heinz <extern.german_heinz@allianz.com>
2026-07-12 19:38:28 +0200

    Rename package from com.gaelvert.springboot to com.example.demo

--- src/test/java/com/example/demo/ApplicationTest.java
+++ src/test/java/com/example/demo/ApplicationTest.java
@@ -1,10 +1,10 @@
-package com.gaelvert.springboot;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootApplicationTest {
+class ApplicationTest {
 
     @Test
     void contextLoads() {
